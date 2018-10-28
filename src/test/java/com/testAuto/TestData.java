d8df872fe581b56c109ba7dfc17aa2e7c30e8b1a
Marina <seredamv92@gmail.com>
2018-10-28 21:20:52 +0200

    ADD 3 folders

---
+++ src/test/java/com/testAuto/TestData.java
@@ -0,0 +1,14 @@
+package com.testAuto;
+
+public class TestData {
+    static String nameTask = "Check create process";
+    static String uploadFolder= "C:\\projects\\com.example\\HelloMaven2\\FilesForTests";
+    static String fileName = "ZIP for Test.zip";
+    static String email = "seredamv92@gmail.com";
+    static String login = "autorob";
+    static String password = "forautotests";
+    static String passwordWrong = "forautotests123";
+    static String siteLink = "http://jira.hillel.it:8080";
+
+
+}
