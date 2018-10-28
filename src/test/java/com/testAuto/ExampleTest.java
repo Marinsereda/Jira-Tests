224ac14f98f5fb39748230d3ffc806c5d23b3fda
Marina <seredamv92@gmail.com>
2018-10-28 21:15:27 +0200

    Refactored code + added "create User" - tests passed

--- src/test/java/com/testAuto/ExampleTest.java
+++ src/test/java/com/testAuto/ExampleTest.java
@@ -1,134 +1,131 @@
 package com.testAuto;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.HashMap;
+import java.io.File;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-public class ExampleTest {
+public class ExampleTest extends TestBase{
 
-    private static WebDriver browser;
-    static Code c;
+//    public static WebDriver browser;
+//    static Code c;
 
-    static String nameTask = "Check create process";
     static String numberTask;
     static String newIssuePath;
-    static String downloadFilepath;
-    static String fileName2;
-    static String fileName = "forTest.jpg";
+    static String uploadFileHref;
+    static String  fileTitle;
+    static File f;
+    static String fileDownloadPAth;
 
 
 
-    public static void loginPass() {
 
-        browser.get("http://jira.hillel.it:8080");
-        c.findAndFill(By.cssSelector("#login-form-username"), "autorob");
-        c.findAndFill(By.cssSelector("#login-form-password"), "forautotests\n");
+    public static void loginPass(boolean correctPass) {
+        String pass = (correctPass? TestData.password: TestData.passwordWrong)+ "\n";
+
+        browser.get(TestData.siteLink);
+        c.findAndFill(By.cssSelector("#login-form-username"), TestData.login);
+        c.findAndFill(By.cssSelector("#login-form-password"), pass );
     }
 
 
-    @BeforeTest
+//    @Test(priority = -1)
+//    public static void loginFailed ()  {
+//        loginPass(false);
+//        Assert.assertTrue(browser.findElements(By.cssSelector("div#usernameerror")).size() > 0);
+//
+//        List<WebElement> buttonProfile = browser.findElements(By.cssSelector("a#header-details-user-fullname"));
+//        Assert.assertFalse(
+//                buttonProfile.size() > 0 && buttonProfile.get(0).getAttribute("data-username").equals("autorob"));
+//
+//    }
 
-    public static void openBrowser() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+    @Test
+    public static void testLogin() throws InterruptedException {
+        Thread.sleep(2500);
 
-        downloadFilepath = System.getProperty("user.dir") + "\\downloaded";
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        chromePrefs.put("profile.default_content_settings.popups", 0);
-        chromePrefs.put("download.default_directory", downloadFilepath);
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("prefs", chromePrefs);
-        DesiredCapabilities cap = DesiredCapabilities.chrome();
-        cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-        cap.setCapability(ChromeOptions.CAPABILITY, options);
-        browser = new ChromeDriver(cap);
-
-        c = new Code(browser);
-
-        browser.manage().window().maximize();
-        browser.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        loginPass(true);
+        Thread.sleep(2000);
+        Assert.assertTrue(browser.findElements(By.cssSelector("#header-details-user-fullname")).size() > 0);
     }
 
-
-
-
-    @Test (priority = -1)
-    public static void loginFailed ()  {
-        loginPass();
-        Assert.assertFalse(browser.findElements(By.cssSelector("div#usernameerror")).size() > 0);
-
-    }
-
-    @Test (priority = 1)
-    public static void testLogin()  {
-
-        Assert.assertTrue(browser.findElements(By.cssSelector("a[href=\"/secure/CreateIssue!default.jspa\"]")).size() > 0);
-    }
-
-    @Test (priority = 2)
+    @Test(dependsOnMethods = "testLogin")
 
     public static void testCreateTask() {
 
         browser.findElement(By.cssSelector("a[href=\"/secure/CreateIssue!default.jspa\"]")).click();
-        c.findAndFill(By.cssSelector(".jira-dialog-content #summary"), nameTask);
+        c.findAndFill(By.cssSelector(".jira-dialog-content #summary"), TestData.nameTask);
         browser.findElement(By.cssSelector(".jira-dialog-content #create-issue-submit")).click();
         numberTask = browser.findElement(By.cssSelector(".issue-created-key")).getAttribute("data-issue-key");
         List<WebElement> linkNewIssues = browser.findElements(By.cssSelector("a.issue-created-key"));
         newIssuePath = linkNewIssues.get(0).getAttribute("href");
 
-        Assert.assertTrue(browser.findElements(By.cssSelector(".aui-message-success")).size() > 0&&
-                linkNewIssues.size()!=0);
+        Assert.assertTrue(browser.findElements(By.cssSelector(".aui-message-success")).size() > 0 &&
+                linkNewIssues.size() != 0);
     }
 
-    @Test (priority = 3)
+    @Test(dependsOnMethods = "testCreateTask")
 
     public static void testOpenTask() {
 
         browser.get(newIssuePath);
-        Assert.assertTrue(browser.findElement(By.cssSelector("#summary-val")).getText().contains(nameTask));
+        Assert.assertTrue(browser.findElement(By.cssSelector("#summary-val")).getText().contains(TestData.nameTask));
 
     }
 
 
-    @Test (priority = 4)
+    @Test(dependsOnMethods = "testOpenTask")
 
-    public static void uploadAttach () throws InterruptedException {
+    public static void uploadAttach() throws InterruptedException {
 
-        browser.findElement(By.cssSelector("input.issue-drop-zone__file")).sendKeys(downloadFilepath+"\\"+fileName);
-        Thread.sleep(4000);
+        browser.findElement(By.cssSelector("input.issue-drop-zone__file")).sendKeys(TestData.uploadFolder + "\\" + TestData.fileName);
+Thread.sleep(3000);
+        uploadFileHref = browser.findElement(By.cssSelector(".attachment-title")).getAttribute("href");
+        fileTitle= browser.findElement(By.cssSelector(".attachment-title")).getAttribute("title");
+
+        System.out.println(uploadFileHref + " " + fileTitle);
+
         Assert.assertTrue(browser.findElements(By.cssSelector("div.attachment-thumb")).size() > 0);
     }
 
-    @Test(priority = 5)
-    public static void downloadAttach () throws InterruptedException {
+    @Test(dependsOnMethods = "uploadAttach")
 
-        browser.findElement(By.cssSelector("div.attachment-thumb")).click();
-        Thread.sleep(2000);
-        browser.findElement(By.cssSelector("#cp-control-panel-download")).click();
-        String fileTitle = browser.findElement(By.cssSelector(".attachment-title")).getAttribute("title");
-        browser.findElement(By.cssSelector("#cp-control-panel-close")).click();
-        Thread.sleep(2000);
+    public static void downloadAttach() throws InterruptedException {
 
-        fileName2 = downloadFilepath + "\\"+ fileTitle;
-        Assert.assertTrue(fileName2.contains(fileName));
+            browser.get(uploadFileHref);
+            Thread.sleep(2000);
 
+            fileTitle = fileTitle.substring(0, fileTitle.indexOf(".zip") + 4);
+            fileDownloadPAth = downloadFilepath + "\\" + fileTitle;
+            f = new File(fileDownloadPAth);
+
+            Assert.assertTrue(f.exists());
+        }
+
+
+        @Test (dependsOnMethods = "downloadAttach")
+
+        public static void deleteTest()  {
+            f.delete();
+
+            Assert.assertFalse(f.exists());
+        }
+
+
+
+    @Test(dependsOnMethods = "testLogin", priority = 10)
+    public static void createUser() throws InterruptedException {
+        c.adminUserCreate();
+
+
+        Assert.assertTrue(browser.findElements(By.cssSelector(".user-created-flag-single")).size()>0);
+    }
 
     }
 
-    @AfterTest
-    public static void closeBrowser(){
-        browser.quit();
-    }
 
-}
+
+
